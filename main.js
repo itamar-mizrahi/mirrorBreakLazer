2614ca0864aaa6fdbe9bc8437694de99e59b91b4
Itamar Mizrahi <theitamarmizrahi@gmail.com>
2019-04-17 18:20:45 +0300

    fixed bug attribute width and hieght to canvas

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 
 window.onload = function() {
-    let canvas = document.getElementById("mirrorBreakLazer");
-    let ctx = canvas.getContext("2d");
-    ctx.fillStyle='black';
-    ctx.fillRect(0,0,10,10);
+    let canvas = document.getElementById('mirrorBreakLazer');
+    let ctx = canvas.getContext('2d');
     let img = document.getElementById("spaceship");
-    ctx.drawImage(img, 10, 10);
-
+    ctx.drawImage(img,60,0,60,60);
+    let img2 = document.getElementById("obliquelineleft");
+    ctx.drawImage(img2,0,0,60,60);
 };    
