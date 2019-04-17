f0d6d602549ea269a7e536f07e4fb40a10f63c43
Itamar Mizrahi <theitamarmizrahi@gmail.com>
2019-04-17 18:38:39 +0300

    added all images to canvas with fixed sizes

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-
 window.onload = function() {
+    const x=60;
     let canvas = document.getElementById('mirrorBreakLazer');
     let ctx = canvas.getContext('2d');
-    let img = document.getElementById("spaceship");
-    ctx.drawImage(img,60,0,60,60);
-    let img2 = document.getElementById("obliquelineleft");
-    ctx.drawImage(img2,0,0,60,60);
+    let obliquelineleft = document.getElementById("obliquelineleft");
+    ctx.drawImage(obliquelineleft,0*x,0,60,60);
+    let obliquelineRight = document.getElementById("obliquelineRight");
+    ctx.drawImage(obliquelineRight,1*x,0,60,60);
+    let spaceship = document.getElementById("spaceship");
+    ctx.drawImage(spaceship,2*x,0,60,60);
+    let moon = document.getElementById("moon");
+    ctx.drawImage(moon,3*x,0,60,60);
+ 
 };    
