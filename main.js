dce8d8369fe3ecef5851da089be4342c94e6644b
Itamar Mizrahi <theitamarmizrahi@gmail.com>
2019-04-17 11:05:46 +0300

    changed tag style try img to canvas

---
+++ main.js
@@ -0,0 +1,5 @@
+    let canvas = document.getElementById("mirrorBreakLazer");
+    let ctx = canvas.getContext("2d");
+    let img = document.getElementById("spaceship");
+    ctx.drawImage(img, 10, 10);
+  
