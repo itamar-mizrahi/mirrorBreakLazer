e14c75d9fd225048c786ed2ae67180bec70c3f02
Itamar Mizrahi <theitamarmizrahi@gmail.com>
2019-04-17 13:04:52 +0300

    test entering object to canvas

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+
+window.onload = function() {
     let canvas = document.getElementById("mirrorBreakLazer");
     let ctx = canvas.getContext("2d");
+    ctx.fillStyle='black';
+    ctx.fillRect(0,0,10,10);
     let img = document.getElementById("spaceship");
     ctx.drawImage(img, 10, 10);
-  
+
+};    
