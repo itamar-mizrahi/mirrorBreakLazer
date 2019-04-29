b232d89fc6160622a3f05eaca2430395643540be
Itamar Mizrahi <theitamarmizrahi@gmail.com>
2019-04-29 16:01:01 +0300

    delete objectClass

--- objectClass.js
+++
@@ -1,8 +0,0 @@
-import { Moon } from './moon.js';
-import { Obliquelineleft } from './obliquelineleft.js';
-import { ObliquelineRight } from './obliquelineRight.js';
-import { Spaceship } from './spaceship.js';
-import { Lazer } from './lazer.js';
-import { Levels } from './levels.js';
-
-export class Object
